2a8d25cf15aa0a56ccccc0a26dc3141939121a23
Gijs Groote <gijsgroote@hotmail.com>
2023-12-20 20:33:06 +0100

    first mini gui is alive

---
+++ src/app.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
