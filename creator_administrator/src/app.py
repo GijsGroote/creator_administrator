704d3693ef484b81c4211c1d4348841e12e11628
Gijs Groote <gijsgroote@hotmail.com>
2024-02-09 11:48:04 +0100

    added help menu with some action

--- creator_administrator/src/app.py
+++ creator_administrator/src/app.py
@@ -4,12 +4,16 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6 import uic
+from src.qmessagebox import InfoQMessageBox
 
 class MainWindow(QMainWindow):
 
     def __init__(self, ui_global_path, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi(ui_global_path, self)
+
+        self.showLaserDocumentationAction.triggered.connect(self.openLaserDocumentation)
+        self.show3DPrintDocumentationAction.triggered.connect(self.open3DPrintDocumentation)
 
     def keyPressEvent(self, event):
         ''' Handle shortcuts on main window. '''
@@ -19,29 +23,16 @@ class MainWindow(QMainWindow):
             if event.key() == Qt.Key.Key_Q:
                 self.close()
 
-        # Move around using arrows or vim style
-        # if isinstance(event, QKeyEvent):
-        #     if event.key() == Qt.Key_H or event.key() == Qt.Key_Left:
-        #         self.jobsQTabWidget.toLeftTab()
-
-        #     if event.key() == Qt.Key_L or event.key() == Qt.Key_Right:
-        #         self.jobsQTabWidget.toRightTab()
-
-        #     if event.key() == Qt.Key_K or event.key() == Qt.Key_Up:
-        #         self.jobsQTabWidget.toPreviousRow()
-
-        #     if event.key() == Qt.Key_J or event.key() == Qt.Key_Down:
-        #         self.jobsQTabWidget.toNextRow()
-
-            # shortcut on Enter key
-            if event.key() == Qt.Key.Key_Return:
-                # go through GUI structure to call the itemEnterPressed function the currenlty displayed item
+        # go through GUI structure to call the itemEnterPressed
+        # function the currenlty displayed item
+        if event.key() == Qt.Key.Key_Return:
                 self.jobsQTabWidget.currentWidget().findChild(QStackedWidget).currentWidget().findChild(QListWidget).itemEnterPressed()
-            # shortcut on Esc button
-            # if event.key() == Qt.Key_Escape:
-                
-            #     for child in self.jobsQTabWidget.currentWidget().findChild(QStackedWidget).currentWidget().findChildren(QPushButton):
-            #         print(f"stupid {child.objectName()}")
-            #     print(f"who you {self.jobsQTabWidget.currentWidget().findChild(QStackedWidget).currentWidget().objectName()}")
-            #     print(self.jobsQTabWidget.currentWidget().findChild(QStackedWidget).currentWidget().findChild(BackQPushButton).objectName())
 
+    # TODO: link toward documentation please
+    def open3DPrintDocumentation(self):
+        ''' Open 3D print documentation in web browser. '''
+        InfoQMessageBox(self, 'Documentation dialog not yet implemented')
+
+    def openLaserDocumentation(self):
+        ''' Open Laser documentation in web browser. '''
+        InfoQMessageBox(self, 'Documentation dialog not yet implemented')
