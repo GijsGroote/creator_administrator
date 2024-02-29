daea065d0d638b9a3a4c82664ada95f5d75226e7
Gijs Groote <gijsgroote@hotmail.com>
2024-02-29 13:38:10 +0100

    updated timed message with progress bar

--- creator_administrator/src/qmessagebox.py
+++ creator_administrator/src/qmessagebox.py
@@ -6,100 +6,10 @@ from PyQt6.QtWidgets import (
         QDialog,
         QWidget,
         QMessageBox,
-        QApplication, 
-        QProgressBar,
-        QLabel)
+        QApplication)
 
-from PyQt6.QtCore import Qt, QTimer, QObject, pyqtSignal, pyqtSlot, QRunnable
+from PyQt6.QtCore import Qt, QObject, pyqtSignal, pyqtSlot, QRunnable
 from PyQt6.uic import loadUi
-
-
-class WorkerSignals(QObject):
-    progress = pyqtSignal(int)
-    finished = pyqtSignal()
-
-class JobRunner(QRunnable):
-
-    def __init__(self):
-        super().__init__()
-        self.signals = WorkerSignals()
-
-    @pyqtSlot()
-    def run(self):
-        for n in range(100):
-            self.signals.progress.emit(100-n)
-            time.sleep(0.05)
-        self.signals.finished.emit()
-
-class TimedMessage(QDialog):
-    ''' Short message that can only be clicked away. '''
-
-    def __init__(self, gv: dict, parent: QWidget, text: str, icon=QMessageBox.Icon.Information):
-        super().__init__(parent)
-
-        # DISPLAY THE TIMER WOULD BE NICE, then this should be a QWidget
-        if gv['DISPLAY_TEMP_MESSAGES']:
-
-            loadUi(os.path.join(gv['GLOBAL_UI_DIR'], 'timed_message_dialog.ui'), self)
-            self.textLabel.setText(text)
-
-
-            self.runner = JobRunner()
-            self.runner.signals.progress.connect(self.update_progress)
-            self.runner.signals.finished.connect(self.exit)
-            gv['THREAD_POOL'].start(self.runner)
-
-            # self.setStandardButtons(QMessageBox.StandardButton.NoButton)
-            QShortcut(QKeySequence(Qt.Key.Key_Escape), self).activated.connect(self.exit)
-            # QShortcut(QKeySequence(Qt.Key.Key_Enter), self).activated.connect(self.doNothing)
-
-            self.exec()
-
-    def update_progress(self, n):
-        self.progressBar.setValue(n)
-
-
-    def moveToTopRightOfScreen(self):
-        ''' Move widget to the top right of the screen. '''
-        x = QApplication.desktop().screenGeometry().width() - self.width()
-        self.move(x-100, 100)
-        
-    def moveToTopOfScreen(self):
-        ''' Move widget to top of screen, horizontally centered. '''
-        x = (QApplication.desktop().screenGeometry().width() - self.width()) // 2
-        self.move(x, 100)
-
-    def moveToTopRightCorner(self, parent):
-        parent_geometry = parent.geometry()
-        parent_right_x = parent_geometry.x() + parent_geometry.width()
-        parent_top_y = parent_geometry.y()
-        margin = 0
-
-        self.move(parent_right_x-self.geometry().width()-30-margin, parent_top_y+38+margin)
-
-    def exit(self):
-        self.deleteLater() 
-        self.close()
-
-    def doNothing(self):
-        ''' Literally do nothing. '''
-        
-    def getMainWidget(self, widget):
-        while widget.parent() is not None:
-            print(f'the main widget is now {widget.objectName()}')
-            widget = widget.parent()
-
-        return widget 
-
-class JobFinishedMessageBox(QMessageBox):
-
-    def __init__(self, parent, text, *args, icon=QMessageBox.Icon.Information, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-
-        self.setText(text)
-        self.addButton(QMessageBox.StandardButton.Ok)
-        self.setIcon(icon)
-        self.exec()
 
 class YesOrNoMessageBox(QMessageBox):
 
@@ -120,7 +30,6 @@ class YesOrNoMessageBox(QMessageBox):
         if self.exec()==QMessageBox.StandardButton.Yes:
             return True
         return False
-
 
 
 class InfoQMessageBox(QMessageBox):
@@ -154,6 +63,87 @@ class ErrorQMessageBox(QMessageBox):
         self.addButton(QMessageBox.StandardButton.Ok)
         self.setIcon(QMessageBox.Icon.Critical)
         self.exec()
+
+
+class TimedMessage(QDialog):
+    ''' Short message that can only be clicked away. '''
+
+    def __init__(self, gv: dict, parent: QWidget, text: str):
+        super().__init__(parent)
+
+        if gv['DISPLAY_TEMP_MESSAGES']:
+            loadUi(os.path.join(gv['GLOBAL_UI_DIR'], 'timed_message_dialog.ui'), self)
+            self.textLabel.setText(text)
+            self.progressBar.setStyleSheet("""
+                QProgressBar {
+                 background-color: #C0C6CA;
+                 border: 0px;
+                 padding: 0px;
+                 height: 20px;
+                }
+                QProgressBar::chunk {
+                 background: #7D94B0;
+                 width:5px
+                }
+                """)
+
+            self.runner = ProgressBarRunner()
+            self.runner.signals.progress.connect(self.progressBar.setValue)
+            self.runner.signals.finished.connect(self.exit)
+            gv['THREAD_POOL'].start(self.runner)
+
+            QShortcut(QKeySequence(Qt.Key.Key_Escape), self).activated.connect(self.exit)
+
+            self.exec()
+
+    def moveToTopRightOfScreen(self):
+        ''' Move widget to the top right of the screen. '''
+        x = QApplication.desktop().screenGeometry().width() - self.width()
+        self.move(x-100, 100)
+        
+    def moveToTopOfScreen(self):
+        ''' Move widget to top of screen, horizontally centered. '''
+        x = (QApplication.desktop().screenGeometry().width() - self.width()) // 2
+        self.move(x, 100)
+
+    def moveToTopRightCorner(self, parent):
+        parent_geometry = parent.geometry()
+        parent_right_x = parent_geometry.x() + parent_geometry.width()
+        parent_top_y = parent_geometry.y()
+        margin = 0
+
+        self.move(parent_right_x-self.geometry().width()-30-margin, parent_top_y+38+margin)
+
+    def exit(self):
+        self.deleteLater() 
+        self.close()
+
+    def doNothing(self):
+        ''' Literally do nothing. '''
+        
+    def getMainWidget(self, widget):
+        while widget.parent() is not None:
+            print(f'the main widget is now {widget.objectName()}')
+            widget = widget.parent()
+
+        return widget 
+
+class ProgressBarSignals(QObject):
+    progress = pyqtSignal(int)
+    finished = pyqtSignal()
+
+class ProgressBarRunner(QRunnable):
+
+    def __init__(self):
+        super().__init__()
+        self.signals = ProgressBarSignals()
+
+    @pyqtSlot()
+    def run(self):
+        for n in range(100):
+            self.signals.progress.emit(100-n)
+            time.sleep(0.03)
+        self.signals.finished.emit()
 
 
 
