dbc0601810548324a49908934ddcdc412a98b157
Gijs Groote <gijsgroote@hotmail.com>
2024-03-25 15:02:43 +0100

    updated make_files_bat

--- create_batch_apps.py
+++ create_batch_apps.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 # create batch files to the 3d print and laser application
@@ -5,12 +6,12 @@ if __name__ == '__main__':
 
     # Run the command and capture its output
     poetry_python_path = subprocess.check_output("poetry env info -p", shell=True, text=True).strip()
-    print(f"{poetry_python_path} and again {poetry_python_path}")
+    print(f' {os.path.join(os.path.dirname(os.path.abspath(__file__)))}')
 
-    with open('./laser_app.bat', 'w') as file:
-        file.write(rf'''@ECHO OFF
-"{poetry_python_path}" "creator_administrator/printer/src/printer_app.py"''')
+    with open('laser_app.bat', 'w') as file:
+        file.write(rf'''@echo off
+"{os.path.join(poetry_python_path, 'Scripts', 'python.exe')}" "{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'creator_administrator', 'laser', 'src', 'laser_app.py')}"''')
 
-    with open('./laser_app.bat', 'w') as file:
-        file.write(rf'''@ECHO OFF
-"{poetry_python_path}" "creator_administrator/printer/src/printer_app.py"''')
+    with open('printer_app.bat', 'w') as file:
+        file.write(rf'''@echo off
+"{os.path.join(poetry_python_path, 'Scripts', 'python.exe')}" "{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'creator_administrator', 'printer', 'src', 'printer_app.py')}"''')
