689b8bc77e412790ee46f3f9710ea33ce8014114
Gijs Groote <gijsgroote@hotmail.com>
2024-04-21 16:34:06 +0200

    chagne waginar

--- creator_administrator/test/test_laser_app.py
+++ creator_administrator/test/test_laser_app.py
@@ -7,20 +7,20 @@ import unittest
 
 class TestMyApp(unittest.TestCase):
 
-    # def setUp(self):
-    #     self.laser_app = LaserMainApp([])
-    #     self.laser_window = self.laser_app.build()
+    def setUp(self):
+        self.laser_app = LaserMainApp([])
+        self.laser_window = self.laser_app.build()
 
-    # def test_appExists(self):
-    #     self.assertIsNotNone(self.laser_app)
-    #     self.assertIsNotNone(self.laser_window)
+    def test_appExists(self):
+        self.assertIsNotNone(self.laser_app)
+        self.assertIsNotNone(self.laser_window)
     
     def test_openSettingsDialg(self):
 
         self.assertEqual(1, 1)
 
-    # def tearDown(self):
-        # self.laser_app.quit()
+    def tearDown(self):
+        self.laser_app.quit()
 
 if __name__ == '__main__':
     unittest.main()
